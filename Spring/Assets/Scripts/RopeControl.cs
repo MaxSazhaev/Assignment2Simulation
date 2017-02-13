fc77dc7dbcb3022acc72d80bd3e595ca977ba28e
UndeadKhaos <m323709402@gmail.com>
2017-02-12 22:09:24 -0500

    Updated
    No more errors coming from the spring script

--- Spring/Assets/Scripts/RopeControl.cs
+++ Spring/Assets/Scripts/RopeControl.cs
@@ -5,14 +5,7 @@ using UnityEngine;
 public class RopeControl : MonoBehaviour {
 
     public GameObject ropeShooter;
-    
-
-    private SpringJoint2D rope;
-    public int maxRopeFrameCount;
-    private int ropeFrameCount;
-
     public LineRenderer lineRenderer;
-	// Use this for initialization
 
 	// Update is called once per frame
 	void Update () {
@@ -20,13 +13,8 @@ public class RopeControl : MonoBehaviour {
 	}
     void LateUpdate()
     {
-
         lineRenderer.enabled = true;
-        //lineRenderer.SetVortexCount(2);
         lineRenderer.SetPosition(0, ropeShooter.transform.position);
-        lineRenderer.SetPosition(1, rope.connectedAnchor);
-        
-
     }
 
 
