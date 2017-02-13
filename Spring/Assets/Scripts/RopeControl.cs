c957093fccfa6e2f0e9b7c7ccc269c7612c14172
UndeadKhaos <m323709402@gmail.com>
2017-02-12 21:38:00 -0500

    Assignment

---
+++ Spring/Assets/Scripts/RopeControl.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RopeControl : MonoBehaviour {
+
+    public GameObject ropeShooter;
+    
+
+    private SpringJoint2D rope;
+    public int maxRopeFrameCount;
+    private int ropeFrameCount;
+
+    public LineRenderer lineRenderer;
+	// Use this for initialization
+
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    void LateUpdate()
+    {
+
+        lineRenderer.enabled = true;
+        //lineRenderer.SetVortexCount(2);
+        lineRenderer.SetPosition(0, ropeShooter.transform.position);
+        lineRenderer.SetPosition(1, rope.connectedAnchor);
+        
+
+    }
+
+
+}
